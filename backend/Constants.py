ac14a6bdcc725162e4f209bfad412127e639cf75
AndrewWentzel <tehwentzel@gmail.com>
2020-06-30 10:17:02 -0500

    moved frontend and backend to sub-folders

--- backend/Constants.py
+++ backend/Constants.py
@@ -1,6 +1,6 @@
 class Constants():
 
-    data_root = './data/'
+    data_root = '../resources/'
     county_demographics_file = data_root + 'merged_county_data.json'
     covid_case_file = data_root + 'covid_cases.csv'
     congressional_district_file = data_root + 'congressional_district_data.json'
